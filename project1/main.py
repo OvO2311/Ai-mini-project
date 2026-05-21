ab65cf8b8a59630ec970b66f913b1905150c2a38
Tang Xin Er <tangxiner6879@gmail.com>
2026-05-21 22:48:46 +0800

    Update main.py

--- project1/main.py
+++ project1/main.py
@@ -1,12 +1,18 @@
 print("AI Recommendation System")
 
-user_input = input("What do you like? (movies/food/study): ")
+user_input = input("What do you like? (movies/food/dessert/hobby/novel/study): ")
 
 if user_input == "movies":
-    print("Avengers, Interstellar, John Wick")
+    print("The Shawshank Redemption, Big Fish, The Truman Show, Dream of the Red Chamber")
 elif user_input == "food":
-    print("Nasi Lemak, Ramen, Chicken Rice")
+    print("Wanton Mee, Roti Canai, Satay, Char Kway Teow, Laksa, Bak Kut Teh")
+elif user_input == "dessert":
+    print("Tofu pudding, Chocolate cake, Tiramisu, Cannoli")
+elif user_input == "hobby":
+    print("reading novel, listening music, playing badminton, practicing calligraphy")
+elif user_input == "novel":
+    print("The Vegetarian, To Live, Brothers")
 elif user_input == "study":
-    print("Python, Math, AI Basics")
+    print("Python, AddMath, English, AI Basics")
 else:
     print("No recommendation")
