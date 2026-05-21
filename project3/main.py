e09a8e2f58bcd899c3982cbb1997076226fbc818
Tang Xin Er <tangxiner6879@gmail.com>
2026-05-21 22:51:57 +0800

    Update main.py

--- project3/main.py
+++ project3/main.py
@@ -2,7 +2,7 @@ user = input("Ask me something: ").lower()
 
 if "hello" in user:
     print("Hi there!")
-if "how are you" in user:
+elif "how are you" in user:
     print("I am fine, thank you, and you?")
 elif "name" in user:
     print("I am a simple AI")
